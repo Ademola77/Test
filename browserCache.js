4ca7060e565722697fdf363544f0108c05ef7332
Ademola <44332514+Ademola77@users.noreply.github.com>
2023-05-15 21:03:22 +0100

    Update browserCache.js

--- browserCache.js
+++ browserCache.js
@@ -7,7 +7,7 @@ export async function synchronizeDbWithCache(file) {
 
     const db = window.sqlitedb;
     
-    const backupPath = `/${file}`;
+    const backupPath = `/${xax.db}`;
     const cachePath = `/data/cache${backupPath.split('.')[0]}.db`;
 
     if (!db.init) {
